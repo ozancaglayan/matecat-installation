8f2ca246adb8cd405a4b256286e226225400f225
Ozan Caglayan <ozancag@gmail.com>
2018-02-27 23:23:57 +0100

    add activemq install

--- 01-root.sh
+++ 01-root.sh
@@ -53,3 +53,10 @@ adduser -system activemq
 sed -i "s#activemq:/bin/false#activemq:/bin/bash#g" /etc/passwd
 chown -R activemq: /opt/apache-activemq-5.13.2/
 ln -sf /opt/activemq/bin/activemq /etc/init.d/
+
+/etc/init.d/activemq create /etc/default/activemq
+chown root:nogroup /etc/default/activemq
+chmod 600 /etc/default/activemq
+sed -i 's/managementContext createConnector="false"/managementContext createConnector="true"/g' /opt/activemq/conf/activemq.xml
+ln -s /etc/init.d/activemq /usr/bin/activemq
+/etc/init.d/activemq start
