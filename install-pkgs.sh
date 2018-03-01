0df81e82c604fe86cbfbb144a8f331cbad92ccfa
Ozan Caglayan <ozancag@gmail.com>
2018-03-01 01:24:19 +0100

    update

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -9,7 +9,7 @@ apt install -y monit git curl wget net-tools \
 	apache2 mysql-client mysql-server psmisc htop screen openjdk-8-jre-headless openjdk-8-jdk \
 	redis-server php5.6 php5.6-xdebug php5.6-mysql libapache2-mod-php5.6 \
 	php5.6-curl php5.6-json php5.6-xml php5.6-xml php5.6-zip php5.6-mbstring php5.6-gd \
-	nodejs npm unzip maven
+	nodejs npm unzip maven tree
 
 ln -sf `which nodejs` /usr/bin/node
 
