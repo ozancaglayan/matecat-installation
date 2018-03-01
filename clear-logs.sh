ac667a4fdaae5ca96855c319bc405f1cd58ec9c9
Ozan Caglayan <ozancag@gmail.com>
2018-03-01 22:53:01 +0100

    remove clear-logs

--- clear-logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat /dev/null > /var/log/apache2/matecat.error.log
-rm -rf /home/ozan/MateCat/storage/log_archive/*.log
-rm -rf /home/ozan/MateCat/storage/log_archive/*.txt
-rm -rf /home/ozan/MateCat/nodejs/log/*.log
-rm -rf /home/ozan/MateCat/storage/log_archive/php_errors.txt
-rm -rf /var/log/matecat/*
